e455288cff9381b0525a7a65ccf1d60e17eb8e90
matkijagenkilo <86783808+matkijahenkilo@users.noreply.github.com>
2023-04-09 02:27:48 -0300

    Remove unnecessary process stop

--- autostarter.lua
+++ autostarter.lua
@@ -1,29 +1,9 @@
 local awful = require("awful")
 
-local processesToKill = {
-  "luvit",
-  "discordiabot",
-}
-
-local function killProcesses()
-  for _, process in ipairs(processesToKill) do
-    local handle = io.popen("pgrep " .. process)
-    if handle then
-      local pid = handle:read("*a")
-      handle:close()
-      awful.spawn("kill " .. pid)
-    end
-  end
-end
-
---stop stuff
-do
-  killProcesses()
-  awful.spawn("xautolock -exit")
-  awful.spawn("xautolock -exit")
-  awful.spawn("killall picom")
-  awful.spawn("killall nm-applet")
-end
+awful.spawn("xautolock -exit")
+awful.spawn("xautolock -exit")
+awful.spawn("killall picom")
+awful.spawn("killall nm-applet")
 
 --network gadget
 awful.spawn("nm-applet")
@@ -31,11 +11,12 @@ awful.spawn("nm-applet")
 --keyboard
 awful.spawn("fcitx5")
 
---discord bot w
-awful.spawn("kitty /home/nanako/discordiabot.sh")
-
 --screen locker
 awful.spawn("xautolock -corners +00- -time 5 -locker slock")
 
 --compositor
 awful.spawn("picom --vsync")
+
+--discord bot w
+local handle = io.popen("pgrep luvit")
+if not handle then awful.spawn("kitty /home/nanako/discordiabot.sh") end
